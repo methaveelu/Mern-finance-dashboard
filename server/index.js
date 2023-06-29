5acc8d87bd305f550983d80d33c9217269590a60
methaveelu <110245885+methaveelu@users.noreply.github.com>
2023-06-29 18:12:07 +0800

    mongoDB_backend

--- server/index.js
+++ server/index.js
@@ -12,3 +12,33 @@ const app =express()
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors())
+
+console.log("hello")
+
+//mongoose setup
+
+const PORT = process.env.PORT || 9000
+
+const mongodbUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongodbUrl, {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+console.log(`MongoDB connection to port ${PORT} successful!`);
+});
+
+connection.on('error', (error) => {
+console.log('MongoDB connection error:', error);
+});
+
+
+
